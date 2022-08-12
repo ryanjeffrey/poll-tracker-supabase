0fd567ef17704caa7826f272198f3fba17b95671
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-11 19:26:11 -0500

    Grab DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // import functions and grab DOM elements
+const newPollFormEl = document.getElementById('new-poll-form');
+
+const currentPollQuestion = document.getElementById('current-poll-question');
+const optionAEl = document.getElementById('option-a');
+const optionASubtractButton = document.getElementById('option-a-subtract');
+const optionAVoteCountEl = document.getElementById('option-a-vote-count');
+const optionAAddButton = document.getElementById('option-a-add');
+const optionBEl = document.getElementById('option-b');
+const optionBSubtractButton = document.getElementById('option-b-subtract');
+const optionBVoteCountEl = document.getElementById('option-b-vote-count');
+const optionBAddButton = document.getElementById('option-b-add');
+
+const closePollButton = document.getElementById('close-poll-button');
+
+const pastPollsDivEl = document.getElementById('past-polls-div');
 
 // let state
 
